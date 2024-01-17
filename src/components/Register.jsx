595070ecc71bb332272b2283d3fb93557d881556
Aifert Yet <aifertyet0628@gmail.com>
2024-01-17 22:36:26 +0800

    database registration done

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,68 @@
-import React from "react";
+import React, {useState} from "react";
+import Home from "./Home"
+
 
 function Register(props){
+
+    const [registered, updateRegistered] = useState(false);
+
+
+    async function convertOptions(option){
+        if(option === "option1"){
+            return "Female"
+        }
+        else if (option === "option2"){
+            return "Male"
+        }
+        else{
+            return "Other"
+        }
+    }
+
+    async function handleonSubmit(e){
+        e.preventDefault();
+
+        const firstName = document.getElementById("firstName").value;
+        const lastName = document.getElementById("lastName").value;
+        const gender = document.querySelector('input[name="inlineRadioOptions"]:checked').value;
+        const emailAddress = document.getElementById("emailAddress").value;
+        const password = document.getElementById("password").value;
+
+        var convertedGender = await convertOptions(gender);
+
+        const data = {
+            firstName,
+            lastName,
+            convertedGender,
+            emailAddress,
+            password,
+        };
+
+        try{
+            const response = await fetch("http://localhost:4000/register", {
+                method : "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body : JSON.stringify(data)
+            });
+
+            if(response.ok){
+                console.log("Registered Successfully");
+                updateRegistered(true);
+            }
+            else{
+                console.error("Registration failed");
+            }
+        }
+        catch(error){
+            console.error("Error during Registration:", error);
+        }
+    }
+
     return  (
-        props.show ? 
-            <form>
+        props.show && !registered ? 
+            <form onSubmit = {handleonSubmit}>
                 <div className="row">
                 <div className="col-md-6 mb-4">
                     <div className="form-outline">
@@ -20,9 +79,9 @@ function Register(props){
                 </div>
                 <div className="row">
                 <div className="col-md-6 mb-4 d-flex align-items-center">
-                    <div className="form-outline datepicker w-100">
-                    <input type="text" className="form-control form-control-lg" id="birthdayDate" />
-                    <label htmlFor="birthdayDate" className="form-label">Email</label>
+                <div className="form-outline">
+                    <input type="email" id="emailAddress" className="form-control form-control-lg" />
+                    <label className="form-label" htmlFor="emailAddress">Email</label>
                     </div>
                 </div>
                 <div className="col-md-6 mb-4">
@@ -44,14 +103,14 @@ function Register(props){
                 <div className="row">
                 <div className="col-md-6 mb-4 pb-2">
                     <div className="form-outline">
-                    <input type="email" id="emailAddress" className="form-control form-control-lg" />
-                    <label className="form-label" htmlFor="emailAddress">Password</label>
+                    <input type="password" id="password" className="form-control form-control-lg" />
+                    <label className="form-label" htmlFor="password">Password</label>
                     </div>
                 </div>
                 <div className="col-md-6 mb-4 pb-2">
                     <div className="form-outline">
-                    <input type="tel" id="phoneNumber" className="form-control form-control-lg" />
-                    <label className="form-label" htmlFor="phoneNumber">Repeat Password</label>
+                    <input type="password" className="form-control form-control-lg" />
+                    <label className="form-label" htmlFor="password">Repeat Password</label>
                     </div>
                 </div>
                 </div>
@@ -60,7 +119,6 @@ function Register(props){
                 </div>
             </form>
     : 
-    <></>
     );
 }
 
--- src/server/server.js
+++ src/server/server.js
@@ -4,11 +4,13 @@ import cors from "cors";
 import 'dotenv/config';
 import {createClient} from "pexels";
 import pg from "pg";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 4000;
 
 app.use(cors());
+app.use(bodyParser.json())
 
 const db = new pg.Client({
     user : "postgres",
@@ -21,7 +23,28 @@ db.connect();
 
 const client = createClient(process.env.APIKEY);
 
+async function verifyClient(firstname, lastname){
+    const result = await db.query("SELECT first_name, last_name FROM credentials WHERE first_name = $1 AND last_name = $2", [firstname, lastname]);
 
+    if (result.rows.length === 0){
+        console.log(result.rows);
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+async function registerClient(firstname, lastname, gender, emailAddress, password){
+    try{
+        await db.query("INSERT INTO credentials (first_name, last_name, emailaddress, password, gender) VALUES \
+                        ($1, $2, $3, $4, $5)", [firstname, lastname, emailAddress, password, gender]);
+    }
+    catch(error){
+        console.log("Error registering client", error.message);
+        res.status(500).json({error : "Internal Server Error"});
+    }
+}
 
 app.get("/wallpaper/:country", (req, res) => {
     try{
@@ -45,6 +68,32 @@ app.get("/wallpaper/:country", (req, res) => {
     catch(err){
         console.log("Error fetching wallpaper");
         res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+app.post("/register", async (req, res) => {
+    const {firstName, lastName, gender, emailAddress, password} = req.body;
+
+    const result = await verifyClient(firstName, lastName);
+
+    console.log(result);
+
+    if(result){
+        console.log("Registering User");
+        try{
+            await registerClient(firstName, lastName, gender, emailAddress, password);
+
+            console.log(`${firstName} ${lastName} has been successfully registered`);
+            res.status(200).json({ message: "Registration successful" });
+        }
+        catch(error){
+            console.log("Error registering user");
+            res.status(500).json({error : "Internal Server Error"});
+        }
+    }
+    else{
+        console.log("User already exists");
+        res.status(500).json({error : "Internal Server Error"});
     }
 })
 
