4268f9ce5e1aae97b7b0f248f677002c644f248a
Aifert Yet <aifertyet0628@gmail.com>
2024-01-17 21:31:00 +0800

    starting database for login credentials

--- src/server/server.js
+++ src/server/server.js
@@ -3,13 +3,24 @@ import axios from "axios";
 import cors from "cors";
 import 'dotenv/config';
 import {createClient} from "pexels";
+import pg from "pg";
 
 const app = express();
 const port = 4000;
 
 app.use(cors());
 
+const db = new pg.Client({
+    user : "postgres",
+    host : "localhost",
+    database : "health-app",
+    password : process.env.DBPW,
+    port : process.env.DBPORT,
+});
+db.connect();
+
 const client = createClient(process.env.APIKEY);
+
 
 
 app.get("/wallpaper/:country", (req, res) => {
@@ -18,11 +29,11 @@ app.get("/wallpaper/:country", (req, res) => {
 
         client.photos.search({
             query,
-            per_page : 100
+            per_page : 10
         })
         .then(
             response => {
-                var number = Math.floor(Math.random() * 100)
+                var number = Math.floor(Math.random() * 10)
                 console.log("Fetched");
                 const id = response.photos[number].id;
                 res.json({
