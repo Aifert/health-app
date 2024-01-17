fc9580bf839c1db89660655c85c201919dde05cd
Aifert Yet <aifertyet0628@gmail.com>
2024-01-17 15:02:03 +0800

    note

---
+++ note.js
@@ -0,0 +1,8 @@
+/**
+ * Going to be connecting it to a database, when login need to check if the credentials 
+ * are already available in the database if yes then render to next page
+ * 
+ * If not then straight put to register and after registering then need to put login page again and login again to go into homepage
+ * 
+ * Once logged in, display all the stats
+ */
