d15a5d9c183d60537bfee10b112ed9b4992bb760
Aifert Yet <aifertyet0628@gmail.com>
2024-01-19 15:23:25 +0800

    added exericse and food buttons

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -6,6 +6,8 @@ function CreateArea(props) {
     content: ""
   });
 
+  const [FOOD, setFOOD] = useState(false);
+
   function handleChange(event) {
     const { name, value } = event.target;
 
@@ -26,20 +28,41 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function handleonClick(action){
+    if(action === "food"){
+        setFOOD(true);
+    }
+    else{
+        setFOOD(false);
+    }
+  }
+
   return (
     <div>
       <form>
+        <div className="col-md-6 mb-4">
+                        <h6 className="mb-2 pb-1">What would you like to log today?</h6>
+                        <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="exercise" onClick={() => handleonClick("exercise")} value="option1" />
+                        <label className="form-check-label" htmlFor="femaleGender">Exercise</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="food" value="option2" onClick={() => handleonClick("food")} />
+                        <label className="form-check-label" htmlFor="maleGender">Food
+                        </label>
+                        </div>
+            </div>
         <input
-          name="title"
+          name="date"
           onChange={handleChange}
           value={note.title}
-          placeholder="Title"
+          placeholder="Date"
         />
         <textarea
           name="content"
           onChange={handleChange}
           value={note.content}
-          placeholder="Take a note..."
+          placeholder={FOOD ? "What did you eat today?" : "What did you do today?"}
           rows="3"
         />
         <button onClick={submitNote}>Add</button>
